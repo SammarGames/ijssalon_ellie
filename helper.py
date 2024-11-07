5f7d04ca92e9f665d1d18ddbf159f46cb2d67bdc
Marnix Oppeneer <marnix_oppeneer@hotmail.com>
2024-11-07 11:35:17 +0100

    functies toegevoegd

--- helper.py
+++ helper.py
@@ -6,6 +6,3 @@ print(f"* {tekst} *")
 print(lengte * "*")
 print()
 
-prijzen = {"aardbei" : 3, "vanille" : 4, "chocolade" : 5}
-aanbieding = prijzen["aardbei"] * 0.8
-reclame_tekst = f"Vandaag in de aanbieding: vanille-ijs, 1 liter - slechts € {aanbieding}"
