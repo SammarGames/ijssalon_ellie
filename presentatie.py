46427ffab55daac77d4d834309fa55cc4633966f
Marnix Oppeneer <marnix_oppeneer@hotmail.com>
2024-11-11 13:56:47 +0100

    toegevoegd

---
+++ presentatie.py
@@ -0,0 +1,11 @@
+def presenteer(dictionary,totaal):
+    for key, value in dictionary.items():
+        print(f"{key} : {value} euro")
+    print("=" * 20)
+    print(f"totaal : {totaal} euro")
+
+mijn_dict = {'vis': 10, 'vlees': 25, 'overig': 15}
+totaal = 50
+
+
+   
